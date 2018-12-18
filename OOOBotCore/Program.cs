b3d2ffb78f1d49c5588d9a5edc3d775f82df6a19
Joseph Satlin <jdstalin@gmail.com>
2018-12-17 23:24:48 -0800

    Removed default react cruft

--- OOOBotCore/Program.cs
+++ OOOBotCore/Program.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SayOOOnara.Persistance;
 
 namespace SayOOOnara
 {
@@ -12,11 +15,24 @@ namespace SayOOOnara
 	    public static OooPeriods OooPeriodCollection;
 	    public static Users UserCollection;
 
-
-
-
 	    public static async Task Main(string[] args)
 	    {
+
+		    var context = new OooContext();
+
+		    if (!context.Database.CanConnect())
+		    {
+			    try
+			    {
+				    context.Database.Migrate();
+				}
+			    catch (Exception e)
+			    {
+				    Console.WriteLine("Unable to migrate the database. If this is your first run, please run SayOOOnara under administrative permisions.");
+					Console.WriteLine(e);
+			    }
+				
+		    }
 		    _options = new OptionsFile();
 			OooPeriodCollection = await OooPeriods.Create();
 			UserCollection = new Users();
